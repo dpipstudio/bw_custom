cc94a2d9ad259df548c7f7a6a87e60a780631299
douxxtech <douxx@douxx.tech>
2025-11-17 18:41:08 +0100

    minor changes

--- src/fm_mpx.c
+++ src/fm_mpx.c
@@ -1,8 +1,11 @@
 /*
     PiFmRds - FM/RDS transmitter for the Raspberry Pi
     Copyright (C) 2014 Christophe Jacquet, F8FTK
+    Copyright (C) 2025 douxx@douxx.tech
     
-    See https://github.com/ChristopheJacquet/PiFmRds
+    See: https://github.com/dpipstudio/BWCustom
+
+    Original project: https://github.com/ChristopheJacquet/PiFmRds
     
     rds_wav.c is a test program that writes a RDS baseband signal to a WAV
     file. It requires libsndfile.
--- src/pi_fm_rds.c
+++ src/pi_fm_rds.c
@@ -1,8 +1,97 @@
 /*
- * PiFmRds - FM/RDS transmitter for the Raspberry Pi
- * This program uses the Raspberry Pi's GPIO and DMA hardware to generate
- * FM radio signals. It works by rapidly changing the frequency of a clock
- * signal to encode audio data (frequency modulation).
+ * BotWave Custom - FM/RDS transmitter for the Raspberry Pi
+ * 
+ * Based on PiFmRds by Christophe Jacquet, F8FTK
+ * 
+ * Copyright (C) 2025, douxx@douxx.tech
+ * Copyright (C) 2014, 2015 Christophe Jacquet, F8FTK
+ * Copyright (C) 2012, 2015 Richard Hirst
+ * Copyright (C) 2012 Oliver Mattos and Oskar Weigl
+ *
+ * See: https://github.com/dpipstudio/BWCustom
+ * 
+ * Original project: https://github.com/ChristopheJacquet/PiFmRds
+ *
+ * PI-FM-RDS: RaspberryPi FM transmitter, with RDS.
+ *
+ * This file contains the VHF FM modulator. All credit goes to the original
+ * authors, Oliver Mattos and Oskar Weigl for the original idea, and to
+ * Richard Hirst for using the Pi's DMA engine, which reduced CPU usage
+ * dramatically.
+ *
+ * I (Christophe Jacquet) have adapted their idea to transmitting samples
+ * at 228 kHz, allowing to build the 57 kHz subcarrier for RDS BPSK data.
+ *
+ * To make it work on the Raspberry Pi 2, I used a fix by Richard Hirst
+ * (again) to request memory using Broadcom's mailbox interface. This fix
+ * was published for ServoBlaster here:
+ * https://www.raspberrypi.org/forums/viewtopic.php?p=699651#p699651
+ *
+ * Never use this to transmit VHF-FM data through an antenna, as it is
+ * illegal in most countries. This code is for testing purposes only.
+ * Always connect a shielded transmission line from the RaspberryPi directly
+ * to a radio receiver, so as *not* to emit radio waves.
+ *
+ * ---------------------------------------------------------------------------
+ * These are the comments from Richard Hirst's version:
+ *
+ * RaspberryPi based FM transmitter.  For the original idea, see:
+ *
+ * http://www.icrobotics.co.uk/wiki/index.php/Turning_the_Raspberry_Pi_Into_an_FM_Transmitter
+ *
+ * All credit to Oliver Mattos and Oskar Weigl for creating the original code.
+ *
+ * I have taken their idea and reworked it to use the Pi DMA engine, so
+ * reducing the CPU overhead for playing a .wav file from 100% to about 1.6%.
+ *
+ * I have implemented this in user space, using an idea I picked up from Joan
+ * on the Raspberry Pi forums - credit to Joan for the DMA from user space
+ * idea.
+ *
+ * The idea of feeding the PWM FIFO in order to pace DMA control blocks comes
+ * from ServoBlaster, and I take credit for that :-)
+ *
+ * This code uses DMA channel 0 and the PWM hardware, with no regard for
+ * whether something else might be trying to use it at the same time (such as
+ * the 3.5mm jack audio driver).
+ *
+ * I know nothing much about sound, subsampling, or FM broadcasting, so it is
+ * quite likely the sound quality produced by this code can be improved by
+ * someone who knows what they are doing.  There may be issues realting to
+ * caching, as the user space process just writes to its virtual address space,
+ * and expects the DMA controller to see the data; it seems to work for me
+ * though.
+ *
+ * NOTE: THIS CODE MAY WELL CRASH YOUR PI, TRASH YOUR FILE SYSTEMS, AND
+ * POTENTIALLY EVEN DAMAGE YOUR HARDWARE.  THIS IS BECAUSE IT STARTS UP THE DMA
+ * CONTROLLER USING MEMORY OWNED BY A USER PROCESS.  IF THAT USER PROCESS EXITS
+ * WITHOUT STOPPING THE DMA CONTROLLER, ALL HELL COULD BREAK LOOSE AS THE
+ * MEMORY GETS REALLOCATED TO OTHER PROCESSES WHILE THE DMA CONTROLLER IS STILL
+ * USING IT.  I HAVE ATTEMPTED TO MINIMISE ANY RISK BY CATCHING SIGNALS AND
+ * RESETTING THE DMA CONTROLLER BEFORE EXITING, BUT YOU HAVE BEEN WARNED.  I
+ * ACCEPT NO LIABILITY OR RESPONSIBILITY FOR ANYTHING THAT HAPPENS AS A RESULT
+ * OF YOU RUNNING THIS CODE.  IF IT BREAKS, YOU GET TO KEEP ALL THE PIECES.
+ *
+ * NOTE ALSO:  THIS MAY BE ILLEGAL IN YOUR COUNTRY.  HERE ARE SOME COMMENTS
+ * FROM MORE KNOWLEDGEABLE PEOPLE ON THE FORUM:
+ *
+ * "Just be aware that in some countries FM broadcast and especially long
+ * distance FM broadcast could get yourself into trouble with the law, stray FM
+ * broadcasts over Airband aviation is also strictly forbidden."
+ *
+ * "A low pass filter is really really required for this as it has strong
+ * harmonics at the 3rd, 5th 7th and 9th which sit in licensed and rather
+ * essential bands, ie GSM, HAM, emergency services and others. Polluting these
+ * frequencies is immoral and dangerous, whereas "breaking in" on FM bands is
+ * just plain illegal."
+ *
+ * "Don't get caught, this GPIO use has the potential to exceed the legal
+ * limits by about 2000% with a proper aerial."
+ *
+ *
+ * As for the original code, this code is released under the GPL.
+ *
+ * Richard Hirst <richardghirst@gmail.com>  December 2012
  */
 
 #include <locale.h>
